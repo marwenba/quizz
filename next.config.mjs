e0a93969371f984e4ddeb201af7822015cc8d779
marwenba <marwenbadri@gmail.com>
2024-06-29 00:36:32 +0100

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
-// next.config.js
-module.exports = {
-    // ... rest of the configuration.
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     output: "standalone",
-  };
+};
+
+export default nextConfig;
