2b1b1a2d24d54f0ae0e79454df0e09d2b4edaaa9
Ahmed Rachdi <rachdi_ahmed@outlook.com>
2024-06-29 07:59:25 +0100

    next button

--- app/register/page.js
+++ app/register/page.js
@@ -131,7 +131,7 @@ export default function Register() {
                 maxWidth: "300px",
               }}
             >
-              Suivant
+              Next
             </button>
           </div>
         </form>
