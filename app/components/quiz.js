5d74f4b63dd688d338340c9053aa383991e0815c
Ahmed Rachdi <rachdi_ahmed@outlook.com>
2024-06-29 06:47:52 +0100

    fix timer

--- app/components/quiz.js
+++ app/components/quiz.js
@@ -43,7 +43,7 @@ export default function Quiz({ quizNumber,nextQuiz }) {
       timer = setInterval(() => {
         const elapsedTime = new Date().getTime() - startTime;
         setElapsedTime(elapsedTime);
-      }, 1000);
+      }, 1);
     }
     return () => clearInterval(timer);
   }, [isQuizStarted, startTime]);
